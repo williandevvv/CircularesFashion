624e14bc39ab8437b3a5c12a7a6bddd4093ab17d
MortySanchez <168171846+williandevvv@users.noreply.github.com>
2026-03-04 11:51:30 -0600

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,15 +1,15 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js';
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDub4MtWEFkg4GYzo7b9dVrhI-Ms5OF32I',
-  authDomain: 'circularesfashioncollection.firebaseapp.com',
-  projectId: 'circularesfashioncollection',
-  storageBucket: 'circularesfashioncollection.firebasestorage.app',
-  messagingSenderId: '395491441213',
-  appId: '1:395491441213:web:ceb09a1085e5e13cba0854'
+  apiKey: "AIzaSyDub4MtWEFkg4GYzo7b9dVrhI-Ms5OF32I",
+  authDomain: "circularesfashioncollection.firebaseapp.com",
+  projectId: "circularesfashioncollection", 
+  storageBucket: "circularesfashioncollection.firebasestorage.app", 
+  messagingSenderId: "395491441213",
+  appId: "1:395491441213:web:ceb09a1085e5e13cba0854"
 };
 
 const app = initializeApp(firebaseConfig);
