2cff42fca60f55e3da667d38a1b3a25bf7267f8a
MortySanchez <168171846+williandevvv@users.noreply.github.com>
2026-03-04 10:54:01 -0600

    Update Firebase configuration with new credentials

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -26,12 +26,12 @@ import {
 import { getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-functions.js";
 
 const firebaseConfig = {
-  apiKey: "REEMPLAZAR_API_KEY",
-  authDomain: "REEMPLAZAR_AUTH_DOMAIN",
-  projectId: "REEMPLAZAR_PROJECT_ID",
-  storageBucket: "REEMPLAZAR_STORAGE_BUCKET",
-  messagingSenderId: "REEMPLAZAR_MESSAGING_SENDER_ID",
-  appId: "REEMPLAZAR_APP_ID",
+  apiKey: "AIzaSyDub4MtWEFkg4GYzo7b9dVrhI-Ms5OF32I",
+  authDomain: "circularesfashioncollection.firebaseapp.com",
+  projectId: "circularesfashioncollection",
+  storageBucket: "circularesfashioncollection.firebasestorage.app",
+  messagingSenderId: "395491441213",
+  appId: "1:395491441213:web:ceb09a1085e5e13cba0854",
 };
 
 const app = initializeApp(firebaseConfig);
