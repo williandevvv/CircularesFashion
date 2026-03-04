e3f552f7c55561bf57eba0c510470fc845693679
MortySanchez <168171846+williandevvv@users.noreply.github.com>
2026-03-04 11:02:55 -0600

    Implement upload and delete functions for PDFs
    Refactor PDF upload and delete functionality using Firebase Storage.

--- public/js/storage-adapter.js
+++ public/js/storage-adapter.js
@@ -1,13 +1,16 @@
-import { APP_MODE } from './app-config.js';
+import { storage } from "./firebase-config.js";
+import { ref, uploadBytes, getDownloadURL, deleteObject }
+  from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
-export async function savePdf(file) {
-  if (!file) {
-    return { pdfUrl: null, storagePath: null };
-  }
+export async function uploadPdf(file, circularId) {
+  const path = `circulares/${circularId}/${Date.now()}_${file.name}`;
+  const fileRef = ref(storage, path);
+  await uploadBytes(fileRef, file);
+  const pdfUrl = await getDownloadURL(fileRef);
+  return { pdfUrl, storagePath: path };
+}
 
-  if (APP_MODE.storage === 'local') {
-    return { pdfUrl: null, storagePath: null };
-  }
-
-  return { pdfUrl: null, storagePath: null };
+export async function deletePdfByPath(storagePath){
+  if (!storagePath) return;
+  await deleteObject(ref(storage, storagePath));
 }
